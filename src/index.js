24741cf69ef42c0bfa370661e155e6b1fae5d27f
ka4_ka <klimova_kate@yahoo.com>
2024-10-11 18:49:26 +0300

    init: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let integerStr = n.toString().split("");
+    let result = [];
+    for (let i = integerStr.length-1; i >= 0; i--) {
+      result.push(integerStr[i])
+    }
+    return +result.join("");
 }
