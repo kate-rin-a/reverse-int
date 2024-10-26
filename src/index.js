90980cc8c74f3b6a50c52c5c8e9eb852d2c90170
ka4_ka <137317269+kate-rin-a@users.noreply.github.com>
2024-10-26 15:03:17 +0300

    fix: fixed code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse (n) {
-    let integerStr = n.toString().split("");
+    let integerStr = Math.abs(n).toString().split("");
     let result = [];
     for (let i = integerStr.length-1; i >= 0; i--) {
       result.push(integerStr[i])
